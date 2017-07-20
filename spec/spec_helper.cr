814043ca3f8e37f2fae91770525765ae750ba434
Dru Jensen <drujensen@gmail.com>
2017-07-19 17:53:27 -0700

    initial commit

---
+++ spec/spec_helper.cr
@@ -0,0 +1,3 @@
+require "spec"
+require "amber"
+require "../src/amber-spec"
---
+++ src/amber-spec.cr
@@ -0,0 +1,43 @@
+require "spec"
+require "amber"
+
+class Global
+  @@response : HTTP::Client::Response?
+
+  def self.response=(@@response)
+  end
+
+  def self.response
+    @@response
+  end
+end
+
+{% for method in %w(get head post put patch delete) %}
+  def {{method.id}}(path, headers : HTTP::Headers? = nil, body : String? = nil)
+    request = HTTP::Request.new("{{method.id}}".upcase, path, headers, body )
+    request.headers["Content-Type"] = Amber::Router::Params::URL_ENCODED_FORM
+    Global.response = process_request request
+  end
+{% end %}
+
+def process_request(request)
+  io = IO::Memory.new
+  response = HTTP::Server::Response.new(io)
+  context = HTTP::Server::Context.new(request, response)
+  main_handler = build_main_handler
+  main_handler.call context
+  response.close
+  io.rewind
+  client_response = HTTP::Client::Response.from_io(io, decompress: false)
+  Global.response = client_response
+end
+
+def build_main_handler
+  amber = Amber::Server.instance
+  amber.handler.prepare_pipelines
+  amber.handler
+end
+
+def response
+  Global.response.not_nil!
+end
