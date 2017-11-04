5daf9bc6f20c1e17fcb0232e8fcdc4f0f3adc87a
Mark Siemers <mark.siemers@gmail.com>
2017-11-04 02:37:35 -0700

    Remove dependency on AmberSpec::Controller::Request and the class itself

--- src/amber_spec/controller/test.cr
+++ src/amber_spec/controller/test.cr
@@ -1,31 +1,22 @@
-require "./request"
 require "http/server"
 
 module AmberSpec::Controller
   abstract class Test
-    include AmberSpec::Controller
     macro inherited
-
       {% http_read_verbs  = %w(get head) %}
       {% http_write_verbs = %w(post put patch delete) %}
       {% http_verbs       = http_read_verbs + http_write_verbs %}
 
-    	# Content type should be set for request that have body
-    	# get requests do not have body so no need to have content type header
       {% for method in http_verbs %}
         def {{method.id}}(path, headers : HTTP::Headers? = nil, body : String? = nil)
           request = HTTP::Request.new("{{method.id}}".upcase, path, headers, body )
           {% if http_write_verbs.includes? method %}
             request.headers["Content-Type"] = "application/x-www-form-urlencoded"
           {% end %}
-          Request.response = process_request request
+          process_request request
         end
       {% end %}
 
-    end
-
-    def response
-      Request.response.not_nil!
     end
 
     private def process_request(request)
@@ -36,7 +27,7 @@ module AmberSpec::Controller
       response.close
       io.rewind
       client_response = HTTP::Client::Response.from_io(io, decompress: false)
-      Request.response = client_response
+      client_response
     end
 
     # Must implement a handler that responds to `call(context)`
